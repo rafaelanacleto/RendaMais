9d1bd119613fe045c8265669df59195e1b7fdab5
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-04-16 15:42:36 -0300

    Inicial commit

---
+++ RendaMais.Domain/Entities/Cost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RendaMais.Domain.Entities
+{
+    public class Cost : Entity
+    {
+        public string? Name { get; set; }
+        public double Value { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+
+
+    }
+}
---
+++ RendaMais.Infra/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace RendaMais.Infra
+{
+    public class Class1
+    {
+
+    }
+}
